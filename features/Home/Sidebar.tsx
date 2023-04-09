bcb967e9599428781aa3c3658df0a76f6c290341
Neamul01 <neamulhaque2002@gmail.com>
2023-04-10 04:27:15 +0600

    navbar and sidebar done

--- features/Home/Sidebar.tsx
+++ features/Home/Sidebar.tsx
@@ -1,8 +1,20 @@
 import { Aside } from "@mantine/core";
+import Link from "next/link";
 import React, { useState } from "react";
+import { BsLaptop } from "react-icons/bs";
+import {
+  IoBagHandleOutline,
+  IoBasketballOutline,
+  IoCameraOutline,
+  IoShirtOutline,
+} from "react-icons/io5";
+import { TbBabyCarriage, TbDeviceGamepad2 } from "react-icons/tb";
+import { GiCookingPot } from "react-icons/gi";
+import { useRouter } from "next/router";
 
 const Sidebar = () => {
   const [scrolled, setScrolled] = useState(false);
+  const router = useRouter();
 
   const handleScroll = () => {
     if (window.scrollY > 100) {
@@ -18,14 +30,63 @@ const Sidebar = () => {
     return () => window.removeEventListener("scroll", handleScroll);
   }, []);
 
+  const sidebarItems = [
+    { id: 1, name: "Computer and Laptops", icon: <BsLaptop size={"1.8rem"} /> },
+    {
+      id: 2,
+      name: "Computer and Laptops",
+      icon: <IoShirtOutline size={"1.7rem"} />,
+    },
+    {
+      id: 3,
+      name: "Computer and Laptops",
+      icon: <TbDeviceGamepad2 size={"1.8rem"} />,
+    },
+    {
+      id: 4,
+      name: "Computer and Laptops",
+      icon: <IoCameraOutline size={"1.9rem"} />,
+    },
+    {
+      id: 5,
+      name: "Computer and Laptops",
+      icon: <GiCookingPot size={"2rem"} />,
+    },
+    {
+      id: 6,
+      name: "Computer and Laptops",
+      icon: <IoBasketballOutline size={"1.7rem"} />,
+    },
+    {
+      id: 7,
+      name: "Computer and Laptops",
+      icon: <IoBagHandleOutline size={"1.7rem"} />,
+    },
+    {
+      id: 8,
+      name: "Computer and Laptops",
+      icon: <TbBabyCarriage size={"1.7rem"} />,
+    },
+  ];
+
   return (
-    <Aside
-      className={`${
-        scrolled ? "absolute top-10 left-[30rem]" : "sticky left-10 top-8"
-      } bg-red-500 p-4 h-28 w-2 z-50`}
-    >
-      <p>This div will move up when you scroll down!{window.scrollY}</p>
-    </Aside>
+    <>
+      {router.pathname === "/" && (
+        <Aside
+          className={`${
+            scrolled ? "fixed top-20" : "absolute top-40 "
+          } bg-white shadow-2xl p-4 h-[34.25rem] w-20 z-50 left-[13.5rem]`}
+        >
+          <ul className="flex flex-col h-full justify-between text-gray-500 py-2">
+            {sidebarItems.map((item) => (
+              <li className="flex justify-center" key={item.id}>
+                <Link href={"/"}>{item.icon}</Link>
+              </li>
+            ))}
+          </ul>
+        </Aside>
+      )}
+    </>
   );
 };
 
